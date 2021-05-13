ea11736109134a048cb566c0ebdad736b279e383
cayde <Caydencoat@gmail.com>
2021-05-13 08:19:00 -0400

    hello

---
+++ src/main/Otter.java
@@ -0,0 +1,11 @@
+import creatures.AquaticMammal;
+
+public class Otter extends AquaticMammal {
+    public void method1() {
+        System.out.println("Paws");
+    }
+
+    public String toString() {
+      return "Beach Dweller";
+    }
+}
